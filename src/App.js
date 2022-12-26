d908c9e68546c50e0e23dbdfc52a46af3cb0db8d
stephanieonyekaba <onyek009@umn.edu>
2022-12-26 11:00:18 -0700

    drop down and title change

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
+import { FormControl } from '@mui/material';
 import './App.css';
+import {
+  MenuItem,
+  Select
+} from "@mui/material"
 
 function App() {
   return (
     <div className="app">
-     <h1>LETS GOOOOOOO</h1>
+<div className="app__header">
 
+     <h1>COVID TRACKER</h1>
+     <FormControl className='app__dropdwon'> 
+          <Select
+            variant="outlined"
+            value="abc"
+          >
+            <MenuItem value="worldwide">OPTION 1 </MenuItem>
+            <MenuItem value="worldwide">OPTION 2 cases </MenuItem>
+            <MenuItem value="worldwide">OPTION 3 </MenuItem>
 
+          </Select>
+      </FormControl>
+</div>
     {/* //HEADDER
     //TITLE AND SELECT INPUT DROPDOWN
     
@@ -16,7 +33,8 @@ function App() {
     //GRAPH
     //MAP */}
 
-    
+
+
     </div>
   );
 }
