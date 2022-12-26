ba302fcc2cddc4d4f1267f00175a7c2be1d37fd5
stephanieonyekaba <onyek009@umn.edu>
2022-12-26 10:29:00 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+     <h1>LETS GOOOOOOO</h1>
+
+
+    {/* //HEADDER
+    //TITLE AND SELECT INPUT DROPDOWN
+    
+    //INFOBOX
+    //INFOBOX
+
+    //TABLE
+    //GRAPH
+    //MAP */}
+
+    
     </div>
   );
 }
